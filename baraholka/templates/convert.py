cf3839893f650a410a735b7a5269d55089be6e24
thegreenshark <86243930+thegreenshark@users.noreply.github.com>
2022-12-20 22:43:32 +0300

    Доработан скрипт конвертации

--- baraholka/templates/convert.py
+++ baraholka/templates/convert.py
@@ -2,6 +2,8 @@
 
 INPUT_FILE_NAME = 'test.html'
 
+import os
+
 def replace(string, replaceIn, replaceOut):
     modifiedString = string.replace(replaceIn, replaceOut)
     if string == modifiedString:
@@ -85,7 +87,7 @@ while 1:
     pathStartPos = string.find('href="', linkStartPos + len('<link'))
     if pathStartPos == - 1:
         break
-    pathStartPos +=  + len('href="')
+    pathStartPos += len('href="')
     pathEndPos = string.find('"', pathStartPos)
     if pathEndPos == - 1:
         break
@@ -126,6 +128,33 @@ if metasString != '':
 
 
 
+searchStartPos = 0
+while 1:
+    imgStartPos = string.find('<img', searchStartPos)
+    if imgStartPos == -1:
+        break
+
+    imgEndPos = string.find('>', imgStartPos + len('<img'))
+    if imgEndPos == -1:
+        break
+
+    pathStartPos = string.find('src="', imgStartPos + len('<img'))
+    if pathStartPos == -1:
+        break
+    pathStartPos += len('src="')
+    pathEndPos = string.find('"', pathStartPos)
+    if pathEndPos == - 1:
+        break
+    pathEndPos -= 1
+
+
+    string = string[:pathStartPos] + '{{url_for(\'.static\', filename=\'' + string[pathStartPos:pathEndPos+1] + '\')}}' + string[pathEndPos+1:]
+    searchStartPos = pathEndPos + len('{{url_for(\'.static\', filename=\'') + len('\')}}')
+
+
+
+
+
 string = '{% extends "bootstrap/base.html" %}\n\n' + string
 
 
@@ -134,13 +163,17 @@ string = '{% extends "bootstrap/base.html" %}\n\n' + string
 
 
 
-OUT_FILE_NAME = ''
+OUT_FILE_NAME = INPUT_FILE_NAME
+
+rename = ''
 dotPos = INPUT_FILE_NAME.rfind('.')
 if dotPos == -1:
-    OUT_FILE_NAME = INPUT_FILE_NAME + '_'
+    rename = INPUT_FILE_NAME + '_'
 else :
-    OUT_FILE_NAME = INPUT_FILE_NAME[:dotPos] + '_' + INPUT_FILE_NAME[dotPos:]
+    rename = INPUT_FILE_NAME[:dotPos] + '_' + INPUT_FILE_NAME[dotPos:]
 
+
+os.rename(INPUT_FILE_NAME, rename)
 wordsFile = open(OUT_FILE_NAME, 'w',  encoding='utf-8')
 wordsFile.write(string)
 wordsFile.close()
